51a0a72020b99f28f2d7c7faf5005e42fb287233
Cristian Franco <cfrancobedoya@gmail.com>
2021-09-18 23:39:00 -0500

    Adding CRUD to network

--- components/bicicleta/controller.js
+++ components/bicicleta/controller.js
@@ -11,11 +11,54 @@ function addBicycle(name) {
     return store.add(bicycle);
 }
 
+function getBicycleById(filterBicycle) {
+    return new Promise((resolve, reject) => {
+        resolve(store.getById(filterBicycle));
+    });
+}
+
 function listBicycles() {
     return store.list();
 }
 
+function updateBicycleById(id, name, user) {
+    return new Promise(async (resolve, reject) => {
+        if (!id || !name) {
+            reject('Invalid data');
+            return false;
+        }
+
+        const bicycle = {
+            name,
+            user
+        }
+
+        const result = await store.update(id, bicycle);
+
+        resolve(result);
+    })
+}
+
+function deleteBicycleById(id) {
+    return new Promise((resolve, reject) => {
+        if (!id) {
+            reject('Invalid Id');
+            return false;
+        }
+        store.remove(id)
+            .then(() => {
+                resolve();
+            })
+            .catch(e => {
+                reject(e);
+            })
+    });
+}
+
 module.exports = {
     addBicycle,
+    getBicycleById,
     listBicycles,
+    updateBicycleById,
+    deleteBicycleById
 }
--- components/bicicleta/network.js
+++ components/bicicleta/network.js
@@ -3,7 +3,13 @@ const response = require('../../network/response');
 const controller = require('./controller');
 const router = express.Router();
 
-router.post('/', function (req, res) {
+router.post('/', createBicycle);
+router.get('/:id', getBicycleById)
+router.get('/', getBicycles);
+router.put('/:id', updateBicycle);
+router.delete('/:id', deleteBicycle);
+
+function createBicycle(req, res) {
     controller.addBicycle(req.body.name)
         .then(data => {
             response.success(req, res, data, 201);
@@ -11,9 +17,19 @@ router.post('/', function (req, res) {
         .catch(err => {
             response.error(req, res, 'Internal error', 500, err);
         });
-});
+};
 
-router.get('/', function (req, res) {
+function getBicycleById(req, res) {
+    controller.getBicycleById(req.params.id)
+        .then((bicycle) => {
+            response.success(req, res, bicycle, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Unexpected Error', 500, e);
+        });
+}
+
+function getBicycles(req, res) {
     controller.listBicycles()
         .then(bicicletas => {
             response.success(req, res, bicicletas, 200);
@@ -21,6 +37,31 @@ router.get('/', function (req, res) {
         .catch(err => {
             response.error(req, res, 'Internal error', 500, err);
         });
-});
+};
+
+function updateBicycle() {
+    controller.updateBicycleById(req.params.id,
+        req.body.name,
+        req.body.user,
+    )
+        .then((data) => {
+            response.success(req, res, data, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Internal Error', 500, e);
+        });
+}
+
+function deleteBicycle(req, res) {
+    controller.deleteBicycleById(req.params.id)
+        .then(() => {
+            response.success(req, res, `Bicycle ${req.params.id} deleted`, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Internal Error', 500, e);
+        })
+}
+
+
 
 module.exports = router;
